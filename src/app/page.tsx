249fb9d173184f9865c4cd96fe30b29606e684ad
Haolin <haolinyang95@gamil.com>
2025-08-18 17:35:28 -0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default async function Home() {
         </ul>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        test
+        Haolin Yang
       </footer>
     </div>
   );

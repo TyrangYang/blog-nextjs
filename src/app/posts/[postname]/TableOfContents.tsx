74e6c7426c4823da2727ef7a7f7e5199839d6b93
Haolin <haolinyang95@gamil.com>
2025-09-13 22:01:47 -0700

    styling contents item

--- src/app/posts/[postname]/TableOfContents.tsx
+++ src/app/posts/[postname]/TableOfContents.tsx
@@ -11,10 +11,10 @@ export default function TableOfContents({
   const [activeId, setActiveId] = useState<string | null>(headings[0].elID);
 
   useEffect(() => {
-    const handler = () => {
+    const onScroll = () => {
       const TriggerLine = window.innerHeight * 0.3;
 
-      const visibleHeadings = headings
+      const onScreenHeadings = headings
         .map((head) => {
           const el = document.getElementById(head.elID);
           if (!el) return null;
@@ -23,7 +23,7 @@ export default function TableOfContents({
         })
         .filter((h) => h !== null);
 
-      const above = visibleHeadings.filter((h) => h.top <= TriggerLine); // headers above TriggerLine
+      const above = onScreenHeadings.filter((h) => h.top <= TriggerLine); // headers above TriggerLine
 
       if (above.length > 0) {
         const nearest = above.reduce((prev, cur) =>
@@ -31,13 +31,13 @@ export default function TableOfContents({
         ); // the largest number of top means closest to the Trigger Line
         setActiveId(nearest.elID);
       } else {
-        setActiveId(visibleHeadings[0]?.elID || null); // fallback to first
+        setActiveId(onScreenHeadings[0]?.elID || null); // fallback to first
       }
     };
 
-    window.addEventListener('scroll', handler, { passive: true });
+    window.addEventListener('scroll', onScroll, { passive: true });
 
-    return () => window.removeEventListener('scroll', handler);
+    return () => window.removeEventListener('scroll', onScroll);
   }, [headings]);
 
   const smoothScrollTo = (id: string) => {
@@ -47,6 +47,8 @@ export default function TableOfContents({
     }
   };
 
+  const depthMargin = ['', '', 'ml-4', 'ml-8', 'ml-12', 'ml-16'];
+
   return (
     <nav className="fixed test-border right-0 w-60 h-screen overflow-scroll">
       <ul>
@@ -54,7 +56,10 @@ export default function TableOfContents({
           <li
             key={head.elID}
             className={clsx(
-              activeId === head.elID && 'text-cyan-500 font-bold',
+              "before:content-['|'] before:mr-3 before:text-cyan-600 before:font-bold",
+              depthMargin[head.depth] || '',
+              activeId === head.elID &&
+                'text-cyan-500 font-bold before:text-red-400',
             )}
           >
             <a
